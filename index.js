ea804d845235b72725bb56e933949a6b5d512aed
ariel.perezc.22@gmail.com <ariel.perezc.22@gmail.com.com>
2022-04-29 17:28:06 -0400

    testa

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _express = _interopRequireDefault(require("express"));
+var _App = _interopRequireDefault(require("./App"));
 
-var app = (0, _express["default"])();
-app.get('/', function (req, res) {
-  res.status(200).json({
-    message: "Welcome to Node.js & Express"
-  });
-});
-app.listen(process.env.PORT || 3000, function () {
-  return console.log("Listening to port 3000");
+_App["default"].listen(process.env.APP_PORT || 3000, function () {
+  return console.log('Server on port: ' + process.env.APP_PORT);
 });
