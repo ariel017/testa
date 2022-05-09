f4747b1cb2f8cac3fb4e7c00d3d3dd951b6ea2ba
ariel.perezc.22@gmail.com <ariel.perezc.22@gmail.com.com>
2022-05-09 18:15:40 -0400

    20209051815

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 
 var _App = _interopRequireDefault(require("./App"));
 
-_App["default"].listen(process.env.APP_PORT || 3000, function () {
+_App["default"].listen(3000, function () {
   return console.log('Server on port: ' + process.env.APP_PORT);
 });
